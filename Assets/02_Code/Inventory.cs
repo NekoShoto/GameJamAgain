b02dd30fcb284934cd44ccb0c51b0724e2d6b484
NekoShoto <miguel.b.gasparr@gmail.com>
2025-09-22 14:54:59 +0200

    i dont like anastas

--- Assets/02_Code/Inventory.cs
+++ Assets/02_Code/Inventory.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Inventory : MonoBehaviour
+public class Inventory : MonoBehaviour, IPointerDownHandler ,IDragHandler,  IBeginDragHandler , IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private RectTransform rectTransform;
+
+    private void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData) //it tells unity u started draggin
     {
-        
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData) //It tells unit u are dragging
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) //it tells unity u ended dragging
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+    public void OnPointerDown(PointerEventData eventData) // it tells unity u can drag
+    {
+        Debug.Log("OnPointerDown");
     }
 }
+
